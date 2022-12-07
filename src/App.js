4b5fb3f8ad0de1d086b4c7a9c1db99643b1f9692
Taylor King <taylorking@Taylors-MBP.attlocal.net>
2022-12-07 15:22:19 -0500

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [music,setMusic] = useState([])
+
+  const getMusic = () =>{
+    axios.get('http://localhost:3000/music')
+    .then((response) =>setMusic(response.data), (err) => console.log(err))
+    .catch((error) => console.log(error))
+  }
+  const handleCreate = (data) => {
+    axios.post('http://localhost:3000/music', data)
+    .then((response) => {
+       console.log(response)
+       setMusic([...music, response.data])
+    })
+  }
+  
+  
+  const handleEdit = (data) => {
+   axios.put('http://localhost:3000/music/' + data._id, data)
+   .then((response) => {
+      let newMusic = music.map((music) => {
+        return music._id !== data._id? music:data         
+      })
+      setMusic(newMusic)
+   })
+  }
+  
+  const handleDelete = (deletedMusic) => {
+    axios.delete('http://localhost:3000/music/' + deletedMusic._id)
+    .then((response) => {
+     let newMusic = music.filter((music) => {
+        return music._id !== deletedMusic._id
+     })
+    })
 }
 
+
+}
 export default App;
